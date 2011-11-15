bf068e038c1553f3b258193c4d3f6be71f04e76e
Marc Fournier <marc.fournier@camptocamp.com>
2011-11-15 13:05:24 +0100

    file renaming to be compliant with recommended module structure.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,20 +7,3 @@
 class sysctl {
 
 }
-
-define sysctl::set_value(
-	$value
-){
-
- 	exec { "exec_sysctl_${name}":
- 		command => "/sbin/sysctl ${name}=${value}",
- 		refreshonly => true,
- 	}
-
- 	sysctl { $name:
- 		val => "$value",
- 		notify => Exec["exec_sysctl_${name}"],
- 	}
-}
-
-
